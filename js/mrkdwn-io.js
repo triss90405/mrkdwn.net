5732edf3f03314c44302c68f163fcfc4a81a4085
Tristan White <oliver.tristan@gmail.com>
2016-10-21 11:18:11 +0200

    Add markdown compiler

---
+++ js/mrkdwn-io.js
@@ -0,0 +1,18 @@
+function Editor(input, preview) {
+
+  this.update = function () {
+
+    preview.innerHTML = markdown.toHTML(input.value);
+
+  };
+
+  input.editor = this;
+  this.update();
+
+}
+
+var $ = function (id) {
+  return document.getElementById(id);
+};
+
+new Editor($("editor"), $("preview"));
