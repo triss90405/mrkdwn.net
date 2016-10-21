bc8e6bc884dbdd52187c5c48b3b7591aa7614567
Tristan White <oliver.tristan@gmail.com>
2016-10-21 16:49:43 +0200

    Add tab indent to editor

--- js/mrkdwn-io.js
+++ js/mrkdwn-io.js
@@ -1,3 +1,11 @@
+/*
+  Todo
+  - Resize windows(editor and preview)
+  - Manual save button (navigation)
+  - Lorem text
+*/
+
+
 var editorWrapper = document.getElementById('editor');
 var previewWrapper = document.getElementById('preview');
 var characterCount = document.getElementById('characterCount');
@@ -5,6 +13,9 @@ var characterCount = document.getElementById('characterCount');
 var init = function() {
   loadData();
   editor(editorWrapper, previewWrapper);
+  colorScheme();
+  save();
+  indentOnTab();
 };
 
 
@@ -42,14 +53,42 @@ var editor = function(input, preview) {
 };
 
 
+/* Color Scheme */
+var colorScheme = function() {
+  var darkColorTrigger = document.getElementById('dark-color-scheme');
+  var lightColorTrigger = document.getElementById('light-color-scheme');
+  var currentColorScheme = localStorage.getItem('colorScheme');
+  var body = document.body;
+
+  body.classList.add(currentColorScheme);
+
+  darkColorTrigger.addEventListener('click', function() {
+    body.classList.add('dark');
+    localStorage.setItem('colorScheme', 'dark');
+  });
+
+  lightColorTrigger.addEventListener('click', function() {
+    body.classList.remove('dark');
+    localStorage.setItem('colorScheme', 'light');
+  });
+};
+
+
+/* Manual save */
+var save = function() {
+  var saveButton = document.getElementById('save');
+  saveButton.addEventListener('click', function() {
+    localStorage.setItem('markdown', editorWrapper.innerHTML);
+    window.location.reload(true);
+  });
+};
+
+
+/* Tab indent in editor */
+var indentOnTab = function() {
+  var el = document.getElementById('editor');
+  tabIndent.render(el);
+};
+
+
 init();
-
-
-/*
-  Todo
-    - Resize windows(editor and preview)
-    - Color shceme changer
-    - Save color scheme choice to localstorage
-    - Manual save button (navigation)
-    - Info bar (footer)
-*/
